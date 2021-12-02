a5a072802163611cc72e9a0c1d670f89d8452bf5
ruthcaswellsmith <ruth@plannuh.com>
2021-12-02 07:30:02 -0500

    Day 2

---
+++ Day2.py
@@ -0,0 +1,52 @@
+import numpy as np
+
+def part_one(filename=None):
+
+    with open(filename, 'r') as f:
+        data = f.readlines()
+    f.close()
+
+    position = np.zeros(2)
+    num_inputs = len(data)
+    for ind in range(num_inputs):
+
+        data[ind] = data[ind].strip('\n')
+        (direction, amount) = data[ind].split(' ')
+        if direction == 'forward':
+            position[0] += int(amount)
+        elif direction == 'up':
+            position[1] -= int(amount)
+        elif direction == 'down':
+            position[1] += int(amount)
+
+    print(f'The answer is {position.prod()}')
+
+def part_two(filename=None):
+
+    with open(filename, 'r') as f:
+        data = f.readlines()
+    f.close()
+
+    position = np.zeros(3)
+    num_inputs = len(data)
+    for ind in range(num_inputs):
+
+        data[ind] = data[ind].strip('\n')
+        (direction, amount) = data[ind].split(' ')
+        if direction == 'forward':
+            position[0] += int(amount)
+            position[1] += position[2]*int(amount)
+        elif direction == 'up':
+            position[2] -= int(amount)
+        elif direction == 'down':
+            position[2] += int(amount)
+
+    print(f'The answer is {position[0]*position[1]}')
+
+if __name__ == "__main__":
+
+    filename = "input/Day2.txt"
+
+    part_one(filename=filename)
+
+    part_two(filename=filename)
