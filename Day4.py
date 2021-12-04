43e88aa0319432a94197e6a238fca51967715e71
ruthcaswellsmith <ruth@plannuh.com>
2021-12-04 09:25:30 -0500

    Day 4

--- Day4.py
+++ Day4.py
@@ -1,8 +1,7 @@
 import numpy as np
-from scipy import stats
-from functools import reduce
 
 SIZE = 5
+
 
 class Game():
 
@@ -32,22 +31,23 @@ class Game():
         if game_type == '1':
 
             while sum(self.winners) == 0:
-
                 current_number = self.__get_new_number()
                 self.__play_round(current_number)
 
-            print(f'The winning score is {self.boards[self.winners.index(True)].get_score(current_number)}')
+            winning_board = self.boards[self.winners.index(True)]
+            print(f'The winning score is {winning_board.get_score(current_number)}')
 
         elif game_type == '2':
 
             while sum(self.winners) < self.num_boards:
-
                 last_winner = self.winners.index(False)
 
                 current_number = self.__get_new_number()
                 self.__play_round(current_number)
 
-            print(f'The final score of the final board is {self.boards[last_winner].get_score(current_number)}')
+            last_winning_board = self.boards[last_winner]
+            print(f'The final score of the final board is {last_winning_board.get_score(current_number)}')
+
 
 class Board():
 
@@ -61,25 +61,24 @@ class Board():
 
         for i in range(SIZE):
             for j in range(SIZE):
-                if self.squares[i,j] == number:
-                    self.marked[i,j] = 1
+                if self.squares[i, j] == number:
+                    self.marked[i, j] = 1
 
         for i in range(SIZE):
-            if self.marked[i,:].sum() == SIZE or self.marked[:,i].sum() == SIZE:
+            if self.marked[i, :].sum() == SIZE or self.marked[:, i].sum() == SIZE:
                 self.is_winner = True
 
     def get_score(self, current_number):
 
         score = 0
         for i in range(SIZE):
-            score += self.squares[i,:][np.where(self.marked[i,:] == 0)].sum()
+            score += self.squares[i, :][np.where(self.marked[i, :] == 0)].sum()
 
         print(score, current_number)
         return current_number * score
 
 
 def process_file(filename):
-
     with open(filename, 'r') as f:
         data = f.read().rstrip('\n').split('\n')
     f.close()
@@ -94,7 +93,7 @@ def process_file(filename):
 
         squares = []
         for i in range(SIZE):
-            row = [int(data[i][ind:ind+2]) for ind in [j*3 for j in range(SIZE)]]
+            row = [int(data[i][ind:ind + 2]) for ind in [j * 3 for j in range(SIZE)]]
             squares.append(row)
 
         boards.append(Board(np.array(squares).astype(int)))
@@ -105,7 +104,6 @@ def process_file(filename):
 
 
 if __name__ == "__main__":
-
     filename = "input/Day4.txt"
 
     game = process_file(filename)
