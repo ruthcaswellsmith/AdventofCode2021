dd2cdf2b27c0147b45c18705cc80cd965401cdae
ruthcaswellsmith <ruth@plannuh.com>
2021-12-08 07:27:40 -0500

    Day 7

--- Day7.py
+++ Day7.py
@@ -1,54 +1,81 @@
 import numpy as np
 from enum import Enum, auto
+from typing import List
 
 class PART(str, Enum):
 
     PART_ONE = auto()
     PART_TWO = auto()
 
-class Crabs():
+# These are number of segments used by numbers 0-9
+SEGMENTS = [6, 2, 5, 5, 4, 5, 6, 3, 7, 6]
 
-    def __init__(self, positions: np.ndarray):
+UNIQUE_SEGMENTS = [2, 3, 4, 7]
 
-        self.positions = positions
-        self.num_crabs = len(self.positions)
+class Display():
 
-    def get_best_position(self, part: PART):
+    def __init__(self):
 
-        fuels = []
-        for i in range(len(self.positions) + 1):
-            fuels.append(self.__calculate_fuel(i, part))
+        self.numbers = {
+            0: {'a', 'b', 'c', 'e', 'f', 'g'},
+            1: {'c', 'f'},
+            2: {'a', 'c', 'd', 'e', 'g'},
+            3: {'a', 'c', 'd', 'f', 'g'},
+            4: {'b', 'c', 'd', 'f'},
+            5: {'a', 'b', 'd', 'f', 'g'},
+            6: {'b', 'c', 'd', 'e', 'f'},
+            7: {'a', 'c', 'f'},
+            8: {'b', 'b', 'c', 'd', 'e', 'f', 'g'},
+            9: {'a', 'b', 'c', 'd', 'f', 'g'},
+}
 
-        return min(fuels)
 
-    def __calculate_fuel(self, target: int, part: PART):
+class Note():
 
-        diffs = abs(self.positions - target * np.ones(self.num_crabs, dtype=int))
-        return sum(np.vectorize(self.__fuel_cost)(diffs, part))
+    def __init__(self, signal_patterns: List[str], output_values: List[str]):
 
-    @staticmethod
-    def __fuel_cost(n, part: PART):
+        self.signal_patterns = signal_patterns
+        self.output_values = output_values
+
+    def count_segments(self):
+
+        lengths = np.array(list(map(len, self.output_values)))
+        counts = [np.count_nonzero(lengths == num) for num in range(8)]
+        return np.array(counts)
+
+class Notes():
+
+    def __init__(self, notes: List[Note]):
+
+        self.notes = notes
+
+    def get_answer(self, part: PART):
 
         if part == PART.PART_ONE:
-            return n
-        else:
-            return int(n * (n+1) / 2)
+
+            answer = 0
+            for note in self.notes:
+
+                answer += note.count_segments()[UNIQUE_SEGMENTS].sum()
+
+        return answer
 
 def process_file(filename):
 
-    positions = np.loadtxt(filename, dtype=int, delimiter=',')
+    with open(filename, 'r') as f:
+        data = f.read().rstrip('\n').split('\n')
 
-    return positions
+    notes = []
+    for line in data:
+        split_line = line.split('|')
+        notes.append(Note(split_line[0].strip().split(), split_line[1].strip().split()))
+
+    return notes
 
 if __name__ == "__main__":
 
-    filename = "input/Day7.txt"
-    positions = process_file(filename)
+    filename = "input/Day8.txt"
+    notes = Notes(process_file(filename))
 
-    crabs = Crabs(positions)
-
-    fuel = crabs.get_best_position(PART.PART_ONE)
-    print(f'P1: They must spend {fuel} fuel to get to the best position.')
-
-    fuel = crabs.get_best_position(PART.PART_TWO)
-    print(f'P2: They must spend {fuel} fuel to get to the best position.')
+    answer = notes.get_answer(PART.PART_ONE)
+    print(f'The answer to part one is {answer}.')
