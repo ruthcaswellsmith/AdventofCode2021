1a31c327a43d954365f8fdab421d07e1a0b43140
ruthcaswellsmith <ruth@plannuh.com>
2021-12-17 22:03:17 -0500

    Day 16

--- Day16.py
+++ Day16.py
@@ -3,102 +3,117 @@ from enum import Enum, auto
 import numpy as np
 from typing import List
 
-class Part(str, Enum):
-    PART_ONE = auto()
-    PART_TWO = auto()
+class Packet():
 
-def get_bits(char):
-    return bin(int(char, 16))[2:].zfill(4)
+    def __init__(self, bits, message):
 
-class Message():
-
-    def __init__(self, message):
-
+        self.bits = bits
         self.message = message
         self.packets = []
 
+    def value(self):
+
+        if self.type == 4:
+            return self.num
+        elif self.type == 0:
+            return np.sum(np.array([packet.value() for packet in self.packets]))
+        elif self.type == 1:
+            return np.prod(np.array([packet.value() for packet in self.packets]))
+        elif self.type == 2:
+            return np.min(np.array([packet.value() for packet in self.packets]))
+        elif self.type == 3:
+            return np.max(np.array([packet.value() for packet in self.packets]))
+        elif self.type == 5:
+            return 1 if self.packets[0].value() > self.packets[1].value() else 0
+        elif self.type == 6:
+            return 1 if self.packets[0].value() < self.packets[1].value() else 0
+        elif self.type == 7:
+            return 1 if self.packets[0].value() == self.packets[1].value() else 0
+
+    def sum_versions(self):
+
+        sub_packet_versions = 0
+        for packet in self.packets:
+            sub_packet_versions += packet.sum_versions()
+        return self.version + sub_packet_versions
+
     def decode(self):
 
-        while self.message:
+        self.__decode_header()
 
-            version, type, bits = self.__decode_header(self.message[0:2])
-            self.message = self.message[2:]
+        if self.type == 4:
+            self.__decode_literal()
+        else:
+            self.__decode_operator()
 
-            if type == 4:
+    @staticmethod
+    def __get_bits(char):
+        return bin(int(char, 16))[2:].zfill(4)
 
-                literal = Literal(version, type, bits, self.message)
-                self.packets.append(literal)
-                literal.decode()
-                print('hi')
+    def __decode_header(self):
 
-            else:
+        while len(self.bits) < 6:
+            self.bits += self.__get_bits(self.message[0])
+            self.message = self.message[1:]
 
-                operator = Operator(version, type, bits, self.message)
-                self.packets.append(operator)
-                operator.decode()
+        self.version = int(self.bits[0:3], 2)
+        self.type = int(self.bits[3:6], 2)
+        self.bits = self.bits[6:]
 
-    def __decode_header(self, first_two_chars):
 
-        bits = ''
-        for i in range(2):
-            bits += get_bits(first_two_chars[i])
+    def __decode_literal(self):
 
-        version = int(bits[0:3], 2)
-        type = int(bits[3:6], 2)
-
-        return version, type, bits[6:8]
-
-class Literal():
-
-    def __init__(self, version, type, bits, message):
-
-        self.version = version
-        self.type = type
-        self.bits = bits
-        self.message = message
-
-    def decode(self, bits):
-
+        self.tot_bits = 6 + len(self.bits)
         num = ''
         done = False
         while not done:
             while len(self.bits) < 5:
-                self.bits += get_bits(self.message[0])
+                self.bits += self.__get_bits(self.message[0])
+                self.tot_bits += 4
                 self.message = self.message[1:]
             if self.bits[0] == '0':
                 done = True
             num += self.bits[1:5]
             self.bits = self.bits[5:]
-
         self.num = int(num, 2)
+        self.tot_bits -= len(self.bits)
 
+    def __decode_operator(self):
 
-class Operator():
-
-    def __init__(self, version, type, bits, message):
-
-        self.version = version
-        self.bits = bits
-        self.type = type
-        self.message = message
-
-    def decode(self):
+        if not self.bits:
+            self.bits += self.__get_bits(self.message[0])
+            self.message = self.message[1:]
 
         self.length_type_id = self.bits[0]
+        self.tot_bits = 22 if self.length_type_id == '0' else 18
         self.bits = self.bits[1:]
-        self.__process_length_type_id()
-
-
-
-    def __process_length_type_id(self, bits):
 
         num_bits = 15 if self.length_type_id == '0' else 11
         while len(self.bits) < num_bits:
-            self.bits += get_bits(self.message[0])
+            self.bits += self.__get_bits(self.message[0])
             self.message = self.message[1:]
 
-        self.packet_info = int(bits[0:num_bits], 2)
-        self.bits = self.bits[num_bits:]
+        self.length = int(self.bits[0:num_bits], 2)
+        self.bits = self.bits[15:] if self.length_type_id == '0' else self.bits[11:]
+
+        if self.length_type_id == '1':
+            for i in range(self.length):
+                packet = Packet(self.bits, self.message)
+                packet.decode()
+                self.bits, self.message = packet.bits, packet.message
+                self.packets.append(packet)
+                self.tot_bits += packet.tot_bits
+
+        else:
+
+            literal_bits = sum([packet.tot_bits for packet in self.packets])
+            while literal_bits < self.length:
+                packet = Packet(self.bits, self.message)
+                packet.decode()
+                self.bits, self.message = packet.bits, packet.message
+                self.packets.append(packet)
+                literal_bits += packet.tot_bits
+                self.tot_bits += packet.tot_bits
 
 def process_file(filename):
 
@@ -109,9 +124,8 @@ def process_file(filename):
 
 if __name__ == "__main__":
 
-    filename = "input/test.txt"
-    message = Message(process_file(filename))
-    message.decode()
-    print('hi')
-
-#    print(f'The answer is {grid.min_points}.')
+    filename = "input/Day16.txt"
+    packet = Packet("", process_file(filename))
+    packet.decode()
+    print(f'The answer to part one is {packet.sum_versions()}')
+    print('The answer to part two is {packet.value()}')
