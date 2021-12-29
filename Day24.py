d3b634a2edd1a996e932f673a2281f31097528e1
ruthcaswellsmith <ruth@plannuh.com>
2021-12-29 06:43:11 -0500

    Day 24

--- Day24.py
+++ Day24.py
@@ -19,54 +19,38 @@ class Instruction:
 class ALU:
 
     def __init__(self):
-        self.w, self.x, self.y, self.z = 0, 0, 0, 0
+        self.variables={}
+        for var in ['w','x','y','z']:
+            self.variables[var] = 0
 
     def process_program(self, instructions: List[Instruction], input: List[int]):
 
         for step in instructions:
             if step.action == Action.INPUT:
                 var = step.operands[0]
-                self.__assign_value(var, input.pop(0))
+                self.variables[var] = input.pop(0)
             else:
                 var = step.operands[0]
-                val1 = self.__get_value(var)
+                val1 = self.variables[var]
                 op2 = step.operands[1]
-                val2 = self.__get_value(op2) if op2.isalpha() else int(op2)
+                val2 = self.variables[op2] if op2.isalpha() else int(op2)
                 if step.action == Action.ADD:
-                    self.__assign_value(var, val1 + val2)
+                    self.variables[var] = val1 + val2
                 elif step.action == Action.MULTIPLY:
-                    self.__assign_value(var, val1 * val2)
+                    self.variables[var] = val1 * val2
                 elif step.action == Action.DIVIDE:
-                    self.__assign_value(var, int(val1 / val2))
+                    self.variables[var] = int(val1 / val2)
                 elif step.action == Action.MODULO:
-                    self.__assign_value(var, val1 - int(val1 / val2) * val2)
+                    self.variables[var] = val1 - int(val1 / val2) * val2
                 elif step.action == Action.EQUAL:
-                    self.__assign_value(var, 1 if val1 == val2 else 0)
+                    self.variables[var] = 1 if val1 == val2 else 0
+            print(step.action, step.operands, self.variables)
 
-    def __assign_value(self, var, value):
-        if var == 'w':
-            self.w = int(value)
-        elif var == 'x':
-            self.x  = int(value)
-        elif var == 'y':
-            self.y = int(value)
-        else:
-            self.z = int(value)
-
-    def __get_value(self, var):
-        if var == 'w':
-            return self.w
-        elif var == 'x':
-            return self.x
-        elif var == 'y':
-            return self.y
-        else:
-            return self.z
 
     def validate_model_number(self, instructions, model_number):
 
         self.process_program(instructions=instructions, input=[int(c) for c in model_number])
-        return self.z == 0
+        return self.variables['z'] == 0
 
 def process_file(filename):
 
@@ -80,15 +64,28 @@ def process_file(filename):
         data = data[1:]
     return instructions
 
+def decrement(model_number):
+
+    int_model_number = int(model_number)
+    contains_zero = True
+    while contains_zero:
+        int_model_number = int_model_number - 1
+        if '0' not in str(int_model_number):
+            contains_zero = False
+    return str(int_model_number)
+
 if __name__ == "__main__":
 
     filename = 'input/Day24.txt'
     monad = process_file(filename)
 
     alu = ALU()
-    model_number = 99999999999999
-    valid = False
-    while not valid:
-        alu.validate_model_number(instructions=monad, str(model_number))
-        model_number -= 1
-
+    model_number = '19999999999999'
+    alu.validate_model_number(instructions=monad, model_number=model_number)
+    # valid = False
+    # while not valid:
+    #     model_number = decrement(model_number)
+    #     print(model_number)
+    #     alu = ALU()
+    #     if alu.validate_model_number(instructions=monad, model_number=model_number):
+    #         valid = True
